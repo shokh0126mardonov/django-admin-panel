f8c7011d6677af95f0a23b63d44a96c0be9236e2
djumanov <djumanovdev@gmail.com>
2026-01-21 16:57:04 +0500

    Category model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(upload_to='category_images/%Y/%m/%d/')
+    description = models.TextField(blank=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+            
+            counter = 1
+            while Category.objects.filter(slug=self.slug).exists():
+                self.slug = f"{slugify(self.name)}-{counter}"
+                counter += 1
+
+        super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['slug']),
+        ]
+    
